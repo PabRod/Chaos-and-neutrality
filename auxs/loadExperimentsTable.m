8cea9a41422842a10599d3f44d78a3d131b5bfd2
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2018-02-13 10:25:17 +0100

    Updated comments
    In loadExperimentsTable

--- auxs/loadExperimentsTable.m
+++ auxs/loadExperimentsTable.m
@@ -1,13 +1,13 @@
 function table = loadExperimentsTable(filename)
-%LOADEXPERIMENTCASES Generates a table with the experiment parameters from 
+%LOADEXPERIMENTCASES Generates a table with the experiment parameters from
 %a csv file
 %
 %   The parameters of each experiment can be stored in a csv file with the
 %   following format:
 %
-%   id;active;nPreys;nPreds;simTime;stabilTime;steps;lyapTime;lyapPert;reps;compPars
-%   A;true;6;4;1000;1000;3000;100;0.01;3;-1 0.1 0 0.25 1
-%   B;false;12;8;1000;1000;3000;100;0.01;3;-1 0.1 0 0.25 1
+%   id;active;nPreys;nPreds;r;K;g;f;e;H;l;simTime;stabilTime;steps;lyapTime;lyapPert;reps;compPars;results_folder;timeseries_folder
+%   failed;false;6;-4;0.5;10;0.4;1e-5;0.6;2;0.15;1000;1000;3000;100;0.01;3;-1 0.1 0 0.25 1;io/;io/
+%   fast;true;12;8;0.5;10;0.4;1e-5;0.6;2;0.15;5000;5000;5000;100;1e-8;3;-1 0.1 0 0.25 1;io/;io/
 %
 %   Syntax:
 %
@@ -25,8 +25,8 @@ function table = loadExperimentsTable(filename)
 opts = detectImportOptions(filename);
 opts.Delimiter = ';';
 opts.Encoding = 'UTF-8';
-opts = setvartype(opts, {'nPreys', 'nPreds', 'r', 'K', 'g', 'f', 'e', ... 
-                         'H', 'l', 'simTime', 'stabilTime', 'steps', ... 
+opts = setvartype(opts, {'nPreys', 'nPreds', 'r', 'K', 'g', 'f', 'e', ...
+                         'H', 'l', 'simTime', 'stabilTime', 'steps', ...
                          'lyapTime', 'reps'}, 'double');
 opts = setvartype(opts, {'id', 'compPars'}, 'string');
 opts = setvartype(opts, {'results_folder', 'timeseries_folder'}, 'char');
