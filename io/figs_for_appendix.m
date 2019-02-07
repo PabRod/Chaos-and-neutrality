1fce8f1de4a95db31390c4ccaa6d8ad9d0a0d19d
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2019-02-07 11:21:45 +0100

    Removed unused figures

--- io/figs_for_appendix.m
+++ io/figs_for_appendix.m
@@ -45,21 +45,6 @@ set(0, 'DefaultTextInterpreter', 'latex');
 set(0, 'DefaultLegendInterpreter', 'latex');
 set(0, 'DefaultAxesTickLabelInterpreter', 'latex');
 
-%% Appendix: selected slices
-fig1 = figure;
-for i = NSub:-1:1 % Bigger first
-    file = subFiles{i};
-    
-    createFigures(file, 'z1'); hold on;
-    title('');
-end
-lgd = legend(subTitles{3:-1:1});
-lgd.FontSize = 18;
-xlabel('Competition parameter', 'FontSize', 18);
-ylabel('Probability of chaos', 'FontSize', 18);
-set(fig1, 'Units', 'Normalized', 'OuterPosition', [0, 0.04, 1, 0.96]);
-saveas(fig1, '..\paper\img\results.png');
-
 %% Appendix: all slices 2
 fig4 = figure;
 for i = NAll:-1:1 % Bigger first
@@ -77,29 +62,6 @@ lgd.FontSize = 18;
 xlim([-0.8, 0.8]);
 set(fig4, 'Units', 'Normalized', 'OuterPosition', [0, 0.04, 1, 0.96]);
 saveas(fig4, '..\paper\img\results_all.png');
-
-%% Appendix: chaos detection comparer
-fig5 = figure;
-
-subplot(3, 1, 1);
-contourFig(allFiles, 'lyaps');
-title('Using estimated max Lyapunov', 'FontSize', 16);
-xlabel('');
-ylabel('');
-
-subplot(3, 1, 2);
-contourFig(allFiles, 'z12');
-title('Using z1 soft', 'FontSize', 16);
-xlabel('');
-ylabel('Number of species (predators + prey)', 'FontSize', 16);
-
-subplot(3, 1, 3);
-contourFig(allFiles, 'z1');
-title('Using z1 hard', 'FontSize', 16);
-xlabel('Competition parameter', 'FontSize', 14);
-ylabel('');
-set(fig5, 'Units', 'Normalized', 'OuterPosition', [0, 0.04, 0.6, 0.99]);
-saveas(fig5, '..\paper\img\contours_all.png');
 
 %% Appendix biodiversity split in 3
 fig_biodsplitbydynamics = figure;
